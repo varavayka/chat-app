5d0a85af84e1de3e63419dec7b2b386611270b30
varavayka <96190181+varavayka@users.noreply.github.com>
2023-12-13 17:53:43 +0300

    Delete newBackend/src/api directory

--- newBackend/src/api/routes/routes/messengerRoute.js
+++
@@ -1,7 +0,0 @@
-const {Router}  = require('express')
-const messengerController = require('../../controllers/messengerController')
-const middleWareAuthorization = require('../../middleware/middleWareAuthorization')
-const router = Router()
-
-router.get('/messenger',middleWareAuthorization, messengerController)
-module.exports = router
